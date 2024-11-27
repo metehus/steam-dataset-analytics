47ccccb3d99d85ecde5ea9197ddcab8897a16ee3
Guilherme <guiguigo4444@gmail.com>
2024-11-27 19:57:24 -0300

    refactor chart

--- app/charts/most_achievements_top20.py
+++ app/charts/most_achievements_top20.py
@@ -5,23 +5,19 @@ def create_most_achievements_chart_top20(df):
     
     df['achievements'] = pd.to_numeric(df['achievements'], errors='coerce').fillna(0).astype(int)
     
-    top_5_achievements = (
-        df[['name', 'achievements']]
-        .sort_values(by='achievements', ascending=False)
-        .head(20)
-    )
+    top_20_achievements = df.nlargest(20, 'achievements')[['name', 'achievements']]
     
     # Plotar o gráfico
     fig, ax = plt.subplots(figsize=(10, 6))
-    ax.bar(
-        top_5_achievements['name'],
-        top_5_achievements['achievements'],
-        color=plt.cm.viridis.colors[:5]
+    plt.barh(
+        top_20_achievements.iloc[::-1]['name'],
+        top_20_achievements.iloc[::-1]['achievements'],
+        color='skyblue', 
+        edgecolor='black' 
     )
-    ax.set_title('Top 5 Jogos com Mais Conquistas', fontsize=16)
-    ax.set_xlabel('Jogos', fontsize=12)
-    ax.set_ylabel('Número de Conquistas', fontsize=12)
-    ax.tick_params(axis='x', rotation=45, labelsize=10)
-    ax.grid(axis='y', linestyle='--', alpha=0.7)
+    ax.set_title('Top 20 Jogos com Mais Conquistas', fontsize=16)
+    ax.set_xlabel('Número de Conquistas', fontsize=12)
+    ax.set_ylabel('Jogos', fontsize=12)
+    ax.tick_params(axis='y', labelsize=10)
     
     return fig
